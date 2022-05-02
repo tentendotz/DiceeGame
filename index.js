ee275e458dee6300ee7bc9edd536afde8b830361
ttt-net <tenten102matsuyama@gmail.com>
2022-05-03 02:45:39 +0900

    Add judgement

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) +1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector(".container h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector(".container h1").innerHTML = "Player 2 Wins! 🚩";
+} else {
+  document.querySelector(".container h1").innerHTML = "Draw!";
+}
