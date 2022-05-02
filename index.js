c0dc285fd14f0987f0e4b9c126f891bbcf16d45e
ttt-net <tenten102matsuyama@gmail.com>
2022-05-03 02:39:37 +0900

    Update player 1 image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var randomNumber1 = Math.floor(Math.random() * 6) +1; //1-6
 var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
 
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) +1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
