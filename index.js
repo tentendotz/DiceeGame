ba2a8deae7a120f3fb555459df7cdc14dce92197
ttt-net <tenten102matsuyama@gmail.com>
2022-05-02 23:00:54 +0900

    Fix random dice image

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) +1; //1-6
 var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
